37d1dbc2eeaebc928b1fed6b679146c15ac0ace3
Song <soog2008@hotmail.com>
2018-07-10 16:02:20 +0800

    Update checkpodrunning.sh

--- checkpodrunning.sh
+++ checkpodrunning.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-NOTRUNPOD=`kubectl -n wisecloud-controller get pod | grep -v NAME| grep -v Running`;if [ ! -n "$NOTRUNPOD" ];then echo "OK";else echo "$NOTRUNPOD";fi
+#对接zabbix ssh.run item 获取ns下pod状态 做非running pod统计，输出 OK 或 具体pod名称
+#By Song
+NOTRUNPOD=`kubectl -n wisecloud-controller get pod | grep -v NAME| grep -v Running`;
+if [ ! -n "$NOTRUNPOD" ];then 
+echo "OK";
+else echo "$NOTRUNPOD";
+fi
